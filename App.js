ab22d209f3a4d5a703c2e12ab09e791e906df978
noman.ali.abbasy@gmail.com <Engrabbasi1>
2020-06-02 16:41:51 +0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import * as eva from '@eva-design/eva';
+import {
+    ApplicationProvider,
+    Button,
+    Layout,
+    Text,
+    Input, TopNavigation, Divider,
+} from '@ui-kitten/components';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {AppNavigator} from './components/navigation.component';
+
+export const LoginScreen = () => {
+  const [value, setValue] = React.useState();
+  return (
+    <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Input
+        placeholder={'Place your Text'}
+        value={value}
+        onChangeText={(nextValue) => setValue(nextValue)}
+      />
+      <Button appearance={'outline'} title={value}>
+        {value}
+      </Button>
+    </Layout>
+  );
+};
+
+export default () => (
+    <ApplicationProvider {...eva} theme={eva.dark}>
+      <AppNavigator />
+    </ApplicationProvider>
+);
---
+++ Readme.md
@@ -0,0 +1,12 @@
+React Native UI
+https://akveo.github.io/react-native-ui-kitten/
+npm i eva-icons
+
+https://github.com/xotahal/react-native-material-ui/tree/master/docs
+
+To install navigation for react-navigate, Follow the steps
+<ul>
+<li>npm install @react-navigation/native</li>
+<li>npm install react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view</li>
+<li>npm install @react-navigation/stack</li>
+</ul>
